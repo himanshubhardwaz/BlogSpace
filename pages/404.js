c1e30138113e77dfbdf4b69e9a4fb359d5d32a70
himanshu76200 <himanshu76200@gmail.com>
2021-07-29 22:47:40 +0530

    about, 404 pages

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "../components/Layout"
+
+const NotFoundPage = () => {
+    return (
+        <Layout title="Page Not Found">
+            <div className="flex flex-col items-center mt-20">
+                <Image src="/images/logo.png" width={70} height={70}
+                    className="bg-gray-800 rounded-2x" />
+                <h1 className="text-6xl my-5">
+                    Oops!
+                </h1>
+                <h2 className="text-4xl text-gray-400 mb-5">
+                    This Page does not exist.
+                </h2>
+            </div>
+        </Layout>
+    )
+}
+
+export default NotFoundPage
