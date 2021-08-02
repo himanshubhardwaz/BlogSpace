7e4f65225922003d1c7f6b143299d4106806fb71
himanshu76200 <himanshu76200@gmail.com>
2021-08-02 20:19:52 +0530

    get demo post using static props, grey-matter

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
+import fs from 'fs';
+const path = require("path");
+const matter = require('gray-matter');
 import Layout from "../components/Layout"
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
+  console.log(posts)
   return (
     <Layout>
       <h1>Hello World</h1>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map(fileName => {
+    const slug = fileName.replace('.md', '')
+
+    const markdownWithMeta = fs.readFileSync(path.join("posts", fileName), 'utf-8')
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  // console.log(posts)
+
+  return {
+    props: {
+      posts
+    },
+  }
 }
