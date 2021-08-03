a6d87d220a515ad54e081c43aee78cc64057cdab
himanshu76200 <himanshu76200@gmail.com>
2021-08-03 08:58:20 +0530

    posts

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
 import fs from 'fs';
 const path = require("path");
 const matter = require('gray-matter');
+import Link from "next/link"
 import Layout from "../components/Layout"
+import Post from "../components/Post"
 
 export default function HomePage({ posts }) {
-  console.log(posts)
+  // console.log(posts)
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl border-b-4 p-5 font-bold">Latest Posts</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3">
+        {posts.map((post, index) =>
+          <Post post={post} key={index} />
+        )}
+      </div>
+      <Link href="/blog">
+        <a className='block text-center
+          border border-gray-500 text-gray-800
+          rounded-md py-4 my-5 transition
+          duration-500 ease select-none
+          hover:text-white hover:bg-gray-900
+          focus:outline-none focus:shadow-outline
+          w-full'>
+          All Posts
+        </a>
+      </Link>
     </Layout>
   )
 }
